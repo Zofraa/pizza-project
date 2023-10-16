430245d0ed180c3b9b50d18d2c19c67ff4dbcbe0
Zofraa <119346876+Zofraa@users.noreply.github.com>
2023-10-16 22:15:44 +0300

    Добавил TS, строго типизировал один компонент.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Header from './components/Header';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 import NotFound from './pages/NotFound';
@@ -13,7 +12,7 @@ import LayoutOne from './layouts/LayoutOne';
 export function App() {
   return (
     <Routes>
-      <Route path="/" element={<LayoutOne />}>
+      <Route path="/" element={<LayoutOne /*теперь тут находится header и прочее*/ />}>
         <Route path="" element={<Home />} />
         <Route path="cart" element={<Cart />} />
         <Route path="pizza/:id" element={<FullPizza />} />
--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
 
-const FullPizza = () => {
-  const [pizza, setPizza] = React.useState();
+const FullPizza: React.FC = () => {
+  // FC - функциональный компонент
+  const [pizza, setPizza] = React.useState<{
+    imageUrl: string;
+    name: string;
+    price: number;
+  }>();
   const { id } = useParams();
   const navigate = useNavigate();
   console.log(useParams());
 
   React.useEffect(() => {
+    // useEffect не типизируется вообще
     async function fetchPizza() {
       try {
         const { data } = await axios.get('https://65019992736d26322f5bfd85.mockapi.io/items/' + id);
@@ -23,7 +29,7 @@ const FullPizza = () => {
   }, []);
 
   if (!pizza) {
-    return 'загрузка';
+    return <>загрузка...</>;
   }
 
   return (
